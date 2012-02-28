8477163a5b0d2a96146b1f2cf041cafbc931cd8e
Joerg Jacobsen <joerg.jacobsen@mac.com>
2012-02-28 11:02:45 +0100

    Log error messages to console when receiving error type events

--- XPCKit/XPCConnection.m
+++ XPCKit/XPCConnection.m
@@ -129,9 +129,28 @@
     [inMessage setNeedsDirectReply:YES];
     
     NSLog(@"Sending message %@", inMessage);
-    xpc_connection_send_message_with_reply(_connection, inMessage.XPCDictionary, dispatch_get_main_queue(), ^(xpc_object_t inXPCReplyDictionary) {
-        XPCMessage *replyMessage = [XPCMessage messageWithXPCDictionary:inXPCReplyDictionary];
-        replyHandler(replyMessage);
+    xpc_connection_send_message_with_reply(_connection, inMessage.XPCDictionary, dispatch_get_main_queue(), ^(xpc_object_t event) {
+        
+        xpc_type_t type = xpc_get_type(event);
+        // TODO: Provide some more elaborate error handling here
+        // N.b. Apple supports different error handlers with NSXPCConnection in Mountain Lion
+        if (type == XPC_TYPE_ERROR) {
+            if (event == XPC_ERROR_CONNECTION_INVALID) {
+                // The process on the other end of the connection has either
+                // crashed or cancelled the connection. After receiving this error,
+                // the connection is in an invalid state, and you do not need to
+                // call xpc_connection_cancel(). Just tear down any associated state
+                // here.
+                NSLog(@"Connection is invalid");
+            } else if (event == XPC_ERROR_CONNECTION_INTERRUPTED) {
+                // Handle per-connection termination cleanup.
+                NSLog(@"Connection was interrupted");
+            }
+        } else {
+            assert(type == XPC_TYPE_DICTIONARY);
+            XPCMessage *replyMessage = [XPCMessage messageWithXPCDictionary:event];
+            replyHandler(replyMessage);
+        }
     });
 }
 
